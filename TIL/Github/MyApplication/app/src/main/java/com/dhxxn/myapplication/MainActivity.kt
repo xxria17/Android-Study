c2fb9367f795aec567151420cb3495d72736944c
xxria17 <xxrcy32@gmail.com>
2021-09-15 15:14:34 +0900

    change code

--- TIL/Github/MyApplication/app/src/main/java/com/dhxxn/myapplication/MainActivity.kt
+++ TIL/Github/MyApplication/app/src/main/java/com/dhxxn/myapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("hello")
+        println(getString(R.string.hello))
     }
 }
